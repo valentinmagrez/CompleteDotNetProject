9b4218d34970294b9dd79c32573d26c5f7ea6454
valentinmagrez <valentin.magrez@gmail.com>
2019-05-15 22:06:53 +0200

    Ajoutez des fichiers projet.

---
+++ WebAuthent/Config.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace WebAuthent
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("testApi", "My test api")
+            };
+        }
+    }
+}
